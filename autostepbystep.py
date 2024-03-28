8bc764512e4484e5b720d225c3b82ad3fa8ca52f
Bowie Edebohls <bowman@edebohls.com>
2024-03-28 12:53:43 -0700

    Updated script with newer version

--- autostepbystep.py
+++ autostepbystep.py
@@ -10,7 +10,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 
 # gpt setup stuff
-openai.api_key = 'NICETRY'
+openai.api_key = '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'
 messages = [ {"role": "system", "content":  
               "You are a intelligent assistant."} ] 
 # the prompt machine, actually asks the questions
@@ -26,7 +26,7 @@ def askgpt(question):
   return(reply)
   messages.append({"role": "assistant", "content": reply})
 
-# driver stuff, selenium prep, logging into codeeveryday
+# driver stuff, selenium prep, logging into codestepbystep
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--window-size=2400,1200")
@@ -38,7 +38,7 @@ driver.get("https://www.codestepbystep.com/login")
 element = driver.find_element(By.XPATH, '/html/body/div[3]/div[2]/div[1]/div/form/div/div[1]/input')
 element.send_keys("bowiee")
 element = driver.find_element(By.XPATH, '/html/body/div[3]/div[2]/div[1]/div/form/div/div[2]/input')
-element.send_keys("NICETRY")
+element.send_keys("3.141592")
 element = driver.find_element(By.XPATH, '//*[@id="submitbutton"]')
 element.click()
 driver.get("https://www.codestepbystep.com/problem/view/java/parameters/getFirstDigit")
@@ -85,3 +85,37 @@ for i in range(2):
 
 
 print("all done :)")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# original gpt thing, too afraid to delete in case i need a replacement after breaking something 
+'''
+def askgpt(question):   
+  messages.append(
+      {"role": "user", "content": question},
+  )
+  chat = openai.ChatCompletion.create(
+      model = "gpt-3.5-turbo", messages=messages
+  )
+
+  reply = chat.choices[0].message.content
+  print(f"ChatGPT : {reply}")
+  messages.append({"role": "assistant", "content": reply})
+  return(reply)
+  '''
